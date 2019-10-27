47995122086df04731d389723ca930d660c3cd34
Muneeb Ahmed <mahmed@an10.io>
2019-10-27 19:42:31 +0500

    aaa
    aaa

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+// UNIT test begin
+
+describe("smoke test", function() {
+    it("checks equality", function() {
+        assert.equal(true, true);
+    });
+});
